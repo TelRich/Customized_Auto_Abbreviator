7d6b303306c01fcae72e24a9732565eb25e0606e
TelRich <okorogoodrich@gmail.com>
2022-12-06 03:32:38 +0100

    re-modified abbreviator to suit specification

--- abbreviator_script.py
+++ abbreviator_script.py
@@ -38,19 +38,27 @@ def Abbreviator(txt_file):
     """
     def seperate_compound(word):
         vc = []
-        # Loops through the cleaned names and split by space first 
-        for x in word.split():
-            for s in others:
-                # Checking if the charaters in others is in any of the splitted words 
-                if s in x:
-                    # Split the word by that charater found in the word
-                    d=x.split(s)
-                    # Add the splitted word to the list defined above
-                    vc  += d
-                else:
-                    pass
-        # Add word that did not fall in the if statement above to the list.
-        vc.append(x)
+        vc1 = []
+        if len(word.split()) > 1:
+            # Loops through the cleaned names and split by space first 
+            for x in word.split():
+                for s in others:
+                    # Checking if the charaters in others is in any of the splitted words 
+                    if s in x:
+                        # Split the word by that charater found in the word
+                        d=x.split(s)
+                        # Add the splitted word to the list defined above
+                        vc  += d
+                    else:
+                        pass
+            # Add word that did not fall in the if statement above to the list.
+            vc.append(x)
+        elif len(word.split()) == 1:
+            for x in word.split():
+                for s in others:
+                    if s in x:
+                        d=x.split(s)
+                vc+=d
         return vc
 
     """Function to select unique index number from words after removing the first character. 
@@ -90,6 +98,61 @@ def Abbreviator(txt_file):
         all_abbre.append(joined[ind[1]])
         str_abb = ''.join(all_abbre)
         return str_abb
+    
+    values_dict = {
+        'A':25, 'B':8, 'C':8, 'D': 9, 'E':35,
+        'F':7, 'G':9, 'H':7, 'I':25, 'J':3,
+        'K':6, 'L':15, 'M':8, 'N':15, 'O':20,
+        'P':8, 'Q':1, 'R':15, 'S':15, 'T':15, 
+        'U':20, 'V':7, 'W':7, 'X':3, 'Y':7, 'Z':1
+    }
+
+    def low_num_char_selector(string):
+        split = [*string]
+        new_dict = {}
+        for x,y in values_dict.items():
+            if x in split:
+                new_dict[x] = y
+                
+        key_lst = list(new_dict.keys())
+        val_lst = list(new_dict.values())
+        num_lst = []
+        for x in split[1:]:
+            num_lst.append(new_dict[x])
+        num_lst.sort()
+
+        first = num_lst[0]
+        sec = num_lst[1]
+        val1 = val_lst.index(first)
+        val2 = val_lst.index(sec)
+        two = key_lst[val1] + key_lst[val2]
+        
+        arr = []
+        split1 = [*string]
+        split2 = [*two]
+        arr.append(split1.index(split2[0]))
+        arr.append(split1.index(split2[1]))
+        arr.sort()
+        return split1[arr[0]]+split1[arr[1]]
+    
+    def low_num_char_selector1(string):
+        split = [*string]
+        new_dict = {}
+        for x,y in values_dict.items():
+            if x in split:
+                new_dict[x] = y
+                
+        key_lst = list(new_dict.keys())
+        val_lst = list(new_dict.values())
+        num_lst = []
+        for x in split[1:]:
+            num_lst.append(new_dict[x])
+        num_lst.sort()
+
+        first = num_lst[0]
+        val1 = val_lst.index(first)
+        one = key_lst[val1]
+        return one
 
     """Here, all functions created above are put to use to perform the task. 
     """
@@ -97,19 +160,39 @@ def Abbreviator(txt_file):
     new_word = remove_punctuation(txt_file)
     # Empty list to hold the new results, words followed by its abbreviation.
     abb_let = []
+    part = []
     # loop through the clean names, seperate any joined words, and extract letters.
     # Add results to the empty list above.
     for word in new_word:
+        string = ''
         if any(x in word for x in others):
             sep = seperate_compound(word=word)
-            abb = extract_letters(sep)
-            abb_let.append(word)
-            abb_let.append(abb)
+            abb_let.append(sep)
+            if len(sep) > 2:
+                string += sep[0][0] + sep[1][0] + sep[2][0]
+                part.append(word)
+                part.append(string)
+            elif len(sep) == 2:
+                string += sep[0][0] + sep[1][0] + sep[1][1]
+                part.append(word)
+                part.append(string)
         else:
-            abb = extract_letters([word])
-            abb_let.append(word)
-            abb_let.append(abb)
-    
+            each = word.split() 
+            if len(each) > 2: 
+                string += each[0][0] + each[1][0] + each[2][0]
+                part.append(word)
+                part.append(string)
+            elif len(each) == 2:
+                string += each[0][0] + each[1][0] + each[1][1]
+                part.append(word)
+                part.append(string)
+            elif len(each) == 1:
+                chars = each[0]
+                string += each[0][0] + low_num_char_selector(chars)
+                part.append(word)
+                part.append(string)
+    print(part)
+
     # Creating file output name as per requirements
     input_name = txt_file.split('.')[0]
     sur_name = 'Omidiji'
@@ -117,7 +200,7 @@ def Abbreviator(txt_file):
     
     # writing the results to the created file above
     with open(output_name, 'w') as file:
-        file.write('\n'.join(abb_let))  
+        file.write('\n'.join(part)) 
 
 # Script based condition to run if script is in main scope 
 if __name__ == '__main__':
